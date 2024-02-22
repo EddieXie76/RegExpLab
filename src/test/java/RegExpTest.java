eaed16f26b2d9cc9ff23daf9c24711d8640f46d4
eddiexie <eddie_xie@derbysoft.com>
2024-02-22 15:12:21 +0800

    init

---
+++ src/test/java/RegExpTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertTrue;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.Test;
+
+public class RegExpTest {
+
+  @Test
+  public void compile_one_time() {
+    final Pattern pattern = Pattern.compile("test(ing)?_\\w+");
+    for (int i = 0; i < 100_000_000; i++) {
+      final Matcher matcher = pattern.matcher("test_hello");
+      assertTrue(matcher.find());
+    }
+  }
+
+  @Test
+  public void compile_each_time() {
+    for (int i = 0; i < 100_000_000; i++) {
+      final Pattern pattern = Pattern.compile("test(ing)?_\\w+");
+      final Matcher matcher = pattern.matcher("test_hello");
+      assertTrue(matcher.find());
+    }
+  }
+}
